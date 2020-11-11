2ecadef70726ad0e3e71f51b8c91f8c24db2b863
Matthew Magee <matthewmagee05@gmail.com>
2020-11-11 16:50:41 -0600

    added fun video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'kyle-dalal';
+
+  @ViewChild('video') video: ElementRef;
+
+  ngAfterViewInit(): void {
+
+  }
+
+
 }
